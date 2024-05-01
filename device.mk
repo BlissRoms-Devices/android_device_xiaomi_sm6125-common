3478fcdded4ffa8a422f6d49cca92e24a3d6c692
Skyblueborb <tomaszborbely0710@gmail.com>
2024-05-01 21:51:11 +0200

    laurel_sprout: build sensorservice
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- device.mk
+++ device.mk
@@ -345,6 +345,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
     android.hardware.sensors-service.xiaomi-multihal
 
 PRODUCT_PACKAGES += \

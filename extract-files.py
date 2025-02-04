c0a4f66df7cd12868669c7d520650b430e6c5e15
Skyblueborb <tomaszborbely0710@gmail.com>
2025-02-04 21:02:13 +0100

    sm6125-common: Cleanup libprotobuf fixups
    These aren't needed anymore.
    
    Signed-off-by: Skyblueborb <tomaszborbely0710@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -45,10 +45,7 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
         .add_needed('libgui_shim.so'),
-    ('vendor/bin/sensors.qti', 'vendor/lib64/libsensorcal.so'): blob_fixup()
-        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so'),
-    ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib/mediadrm/libwvdrmengine.so'): blob_fixup()
-        .replace_needed('libprotobuf-cpp-lite-3.9.1.so', 'libprotobuf-cpp-full-3.9.1.so')
+    ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
     'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
         .add_line_if_missing('gettid: 1'),
